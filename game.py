94df6d7aef467e52280c1ea0c8f9cd9659b82a04
yanjin1210 <yanjin1210@gmail.com>
2021-09-16 16:41:19 -0500

    Changed naming, added docstrings

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
+"""
+game class provide apis for other modules to make change to the game board
+"""
+
 from random import randrange
+from time import sleep
 from termcolor import colored
-import time
 
 MOVES = ["X", "O"]
 
@@ -12,91 +16,167 @@ COLORS = {
 DASHED_LINE = "-------------"
 
 END_MESSAGES = {
-    "O": colored("O", "blue") + " wins",
-    "X": colored("X", "red") + " wins",
-    " ": "Draw"
+    "O": colored("O wins", "blue"),
+    "X": colored("X wins", "red"),
+    " ": colored("Draw", "yellow")
 }
 
 
 class Game:
+    """
+    define the game board handling game board rendering and game moves
+    provide ai solvers for other modules to utilize
+    """
     def __init__(self):
         self.board = [[i * 3 + j + 1 for j in range(3)] for i in range(3)]
-        self.curMove = "X"
-        self.nextMove = "O"
-        self.isEnded = False
+        self.cur_move = "X"
+        self.next_move = "O"
+        self.is_ended = False
         self.moves = []
 
-    def makeMove(self, row, col):
+    def make_move(self, row, col):
+        """
+        make a move in specific cell
+        and make changes to other game status
+
+        :param row: row number of cell
+        :param col: column number of cell
+        :return: None
+        """
         if self.board[row][col] in MOVES:
             print("Cell already occupied.")
         else:
-            self.board[row][col] = self.curMove
-            self.curMove, self.nextMove = self.nextMove, self.curMove
+            self.board[row][col] = self.cur_move
+            self.cur_move, self.next_move = self.next_move, self.cur_move
             self.moves.append((row, col))
 
-    def clearMove(self, row, col):
-        self.board[row][col] = row * 3 + col + 1
-        self.curMove, self.nextMove = self.nextMove, self.curMove
-        self.moves.pop()
-        self.isEnded = False
+    def clear_move(self, row, col):
+        """
+        clear the move at a specific cell
+        and make changes to other game status
 
-    def printRow(self, row):
+        :param row: row number of cell
+        :param col: col number of cell
+        :return: None
+        """
+        self.board[row][col] = row * 3 + col + 1
+        self.cur_move, self.next_move = self.next_move, self.cur_move
+        self.moves.pop()
+        self.is_ended = False
+
+    def print_row(self, row):
+        """
+        print a specific row in format
+        :param row: row number
+        :return: None
+        """
         print("| ", end="")
         for j in range(3):
             move = self.board[row][j]
             print(colored(move, COLORS.get(move, "white")) + " | ", end="")
         print("")
 
-    def printBoard(self):
+    def print_board(self):
+        """
+        render the whole game board
+        :return: None
+        """
         print("")
         print(DASHED_LINE)
         for i in range(3):
-            self.printRow(i)
-            print(DASHED_LINE + "\n")
+            self.print_row(i)
+            print(DASHED_LINE)
+        print("")
 
-    def checkRowWin(self, row):
+    def is_row_winning(self, row) -> bool:
+        """
+        check if a row has the same move ("X" or "O")
+        :param row: row number
+        :return: bool
+        True: the row is of the same move
+        False: the row is not of the same move
+        """
         for j in range(3):
-            if self.board[row][j] != self.nextMove:
+            if self.board[row][j] != self.next_move:
                 return False
         return True
 
-    def checkColWin(self, col):
+    def is_col_winning(self, col):
+        """
+        check if a column has the same move ("X" or "O")
+        :param col: column number
+        :return: bool
+        True: the column is of the same move
+        False: the column is not of the same move
+        """
         for i in range(3):
-            if self.board[i][col] != self.nextMove:
+            if self.board[i][col] != self.next_move:
                 return False
         return True
 
-    def checkLeftDiagWin(self):
+    def is_left_diag_winning(self):
+        """
+        check if a left diagonal has the same move ("X" or "O")
+        :return: bool
+        True: the left diagonal is of the same move
+        False: the left diagonal is not of the same move
+        """
         for i in range(3):
-            if self.board[i][i] != self.nextMove:
+            if self.board[i][i] != self.next_move:
                 return False
         return True
 
-    def checkRightDiagWin(self):
+    def is_right_diag_winning(self):
+        """
+        check if a right diagonal has the same move ("X" or "O")
+        :return: bool
+        True: the right diagonal is of the same move
+        False: the right diagonal is not of the same move
+        """
         for i in range(3):
-            if self.board[i][2-i] != self.nextMove:
+            if self.board[i][2-i] != self.next_move:
                 return False
         return True
 
-    def checkDiagWin(self, row, col):
+    def is_diag_winning(self, row, col):
+        """
+        check if the diagonals with the cell have the same move
+        :param row: row number of the cell
+        :param col: column number of the cell
+        :return: bool
+        True: at least one diagonal is of the same move
+        False: the cell does not belong the a diagonal or diagonals is not of the same move
+        """
         result = False
         if row == col:
-            result |= self.checkLeftDiagWin()
+            result |= self.is_left_diag_winning()
         if row + col == 2:
-            result |= self.checkRightDiagWin()
+            result |= self.is_right_diag_winning()
         return result
 
-    def getResult(self, row, col):
-        if self.checkRowWin(row) or self.checkColWin(col) or self.checkDiagWin(row, col):
-            self.isEnded = True
-            return END_MESSAGES[self.nextMove]
-        elif len(self.moves) == 9:
-            self.isEnded = True
-            return END_MESSAGES[" "]
-        else:
-            return None
+    def get_result(self, row, col):
+        """
+        check and update game status after a move
 
-    def getCandidates(self):
+        :param row: row number of the cell
+        :param col: column number of the cell
+        :return:
+        str: game end message
+        None: game is still going on
+        """
+        if self.is_row_winning(row) or self.is_col_winning(col) or self.is_diag_winning(row, col):
+            self.is_ended = True
+            return END_MESSAGES[self.next_move]
+        if len(self.moves) == 9:
+            self.is_ended = True
+            return END_MESSAGES[" "]
+        return None
+
+    def get_candidates(self) -> [(int, int)]:
+        """
+        get the empty cells
+        :return: [(row, col)]
+        """
         candidates = []
         for i in range(3):
             for j in range(3):
@@ -104,74 +184,130 @@ class Game:
                     candidates.append((i, j))
         return candidates
 
-    def isWinningMove(self, row, col):
-        self.makeMove(row, col)
-        res = self.getResult(row, col)
-        self.clearMove(row, col)
-        return True if res else False
+    def is_winning_move(self, row, col) -> bool:
+        """
+        check if making the move will win or draw the game
+        :param row: row number of cell
+        :param col: column number of cell
+        :return: bool
+        True: this is a winning or drawing move
+        False: this is not
+        """
+        self.make_move(row, col)
+        res = self.get_result(row, col)
+        self.clear_move(row, col)
+        return bool(res)
 
-    def isLosingMove(self, row, col):
-        self.curMove, self.nextMove = self.nextMove, self.curMove
-        res = self.isWinningMove(row, col)
-        self.curMove, self.nextMove = self.nextMove, self.curMove
+    def is_losing_move(self, row, col) -> bool:
+        """
+        check if this is the winning move for your opponent
+        :param row: row number of cell
+        :param col: column number of cell
+        :return: bool
+        """
+        self.cur_move, self.next_move = self.next_move, self.cur_move
+        res = self.is_winning_move(row, col)
+        self.cur_move, self.next_move = self.next_move, self.cur_move
         return res
 
-    def solverEasy(self, candidates):
+    def solver_easy(self, candidates) -> (int, int):
+        """
+        simple ai solver will return a random move
+        :param candidates: empty cells
+        :return: row, col
+        """
         return candidates[randrange(len(candidates))]
 
-    def solverMid(self, candidates):
-        winningMoves = []
-        losingMoves = []
-        neutralMoves = []
+    def solver_mid(self, candidates) -> (int, int):
+        """
+        medium ai solver will check
+        if there is a immediate winning or losing move and return the move
+        if not return random move
+        :param candidates: empty cells
+        :return: row, col
+        """
+        winning_moves = []
+        losing_moves = []
+        neutral_moves = []
+
         for row, col in candidates:
-            if self.isWinningMove(row, col):
-                winningMoves.append((row, col))
-            elif self.isLosingMove(row, col):
-                losingMoves.append((row, col))
-            else: neutralMoves.append((row, col))
-        if len(winningMoves):
-            return self.solverEasy(winningMoves)
-        elif len(losingMoves):
-            return self.solverEasy(losingMoves)
-        else: return self.solverEasy(neutralMoves)
+            if self.is_winning_move(row, col):
+                winning_moves.append((row, col))
+            elif self.is_losing_move(row, col):
+                losing_moves.append((row, col))
+            else:
+                neutral_moves.append((row, col))
 
-    def solverHard(self, candidates):
+        if winning_moves:
+            return self.solver_easy(winning_moves)
+        if losing_moves:
+            return self.solver_easy(losing_moves)
+        return self.solver_easy(neutral_moves)
+
+    def solver_hard(self, candidates):
+        """
+        unbeatable ai solver utilize minmax algorithm to get the best move.
+        :param candidates: empty cells
+        :return: row, col
+        """
         if len(self.moves) == 0:
-            return self.solverEasy(candidates)
-        elif len(self.moves) == 8:
+            return self.solver_easy(candidates)
+        if len(self.moves) == 8:
             return candidates[0]
-        else:
-            score = -1
-            resultRow, resultCol = candidates[0]
-            for row, col in candidates:
-                s = self.minMax(row, col)
-                if s == 1:
-                    return row, col
-                elif s > score:
-                    resultRow, resultCol = row, col
-                    score = s
-            return resultRow, resultCol
+        return self.use_min_max(candidates)
 
-    def minMax(self, row, col) -> int:
+    def min_max(self, row, col) -> int:
+        """
+        minmax algorithm to evaluate a certain move
+        :param row: row number
+        :param col: column number
+        :return: score
+        1: this move could win the game
+        0: this move could at least draw the game
+        -1: this move could lose the game
+        """
         score = 1
-        self.makeMove(row, col)
-        res = self.getResult(row, col)
-        if res == "Draw": score = 0
-        elif res: score = 1
+        self.make_move(row, col)
+        res = self.get_result(row, col)
+        if res == "Draw":
+            score = 0
+        elif res:
+            score = 1
         else:
-            for r, c in self.getCandidates():
-                score = min(score, -self.minMax(r, c))
-        self.clearMove(row, col)
+            for r, c in self.get_candidates():
+                score = min(score, - self.min_max(r, c))
+        self.clear_move(row, col)
         return score
 
-    def solver(self, diff):
-        print("AI is making a move...")
-        time.sleep(2)
-        candidates = self.getCandidates()
-        if diff == 1:
-            return self.solverEasy(candidates)
-        if diff == 2:
-            return self.solverMid(candidates)
-        if diff == 3:
-            return self.solverHard(candidates)
+    def use_min_max(self, candidates) -> (int, int):
+        """
+        choose the cells in candidates that will score the highest
+        :param candidates: empty cells
+        :return: row, col
+        """
+        score = -1
+        result_row, result_col = candidates[0]
+        for row, col in candidates:
+            s = self.min_max(row, col)
+            if s == 1:
+                return row, col
+            if s > score:
+                result_row, result_col = row, col
+                score = s
+        return result_row, result_col
 
+    def solver(self, diff):
+        """
+        display AI thinking message and return the move AI makes based on the difficulty
+        :param diff: difficulty
+        :return: row, col
+        """
+        print("AI is making a move...")
+        sleep(2)
+        candidates = self.get_candidates()
+        if diff == 1:
+            return self.solver_easy(candidates)
+        if diff == 2:
+            return self.solver_mid(candidates)
+        if diff == 3:
+            return self.solver_hard(candidates)
--- play.py
+++ play.py
@@ -1,81 +1,130 @@
+""" This module will interact with player
+    display the game board
+    and let the player to play tic-tac-toe"""
+
 from termcolor import colored
 
 from game import Game
 
 
-def getAI():
+def get_ai() -> bool:
+    """ check if player wants to play with ai:
+        True: play with AI
+        False: play with human
+    """
     option = input("Do you want to play with AI?\n[Yes: Y]/[No: N]: ").strip().lower()
     if option in ["y", "n"]:
         return option == "y"
     print("Invalid input. Please enter \"Y\" or \"N\".")
-    return getAI()
+    return get_ai()
 
 
-def getAITurn():
+def get_ai_turn() -> bool:
+    """ check if AI will play the first turn
+        True: AI first
+        False: play first
+    """
     turn = input("Do you want to play first?\n[Yes: Y]/[No: N]: ").strip().lower()
     if turn in ["y", "n"]:
         return turn == "n"
     print("Invalid input. Please enter \"Y\" or \"N\".")
-    return getAITurn()
+    return get_ai_turn()
 
 
-def getDifficulty():
+def get_difficulty() -> int:
+    """ get the difficulty of AI the player wants to play with:
+        1: easy
+        2: medium
+        3: hard
+    """
     diff = input("Chose the difficulty of AI:\n1. Easy\n2. Medium\n3. Hard\n[1-3]: ").strip()
     if diff in ["1", "2", "3"]:
         return int(diff)
     print("Invalid input. Please choose from 1 to 3.")
-    return getDifficulty()
+    return get_difficulty()
 
 
-def getMove():
+def get_player_move() -> (int, int):
+    """
+    get the position of cell the player wants to play a move
+    :return: row of cell, column of cell
+    0 <= row, col <= 2
+    """
     move = input("Please make your move [1-9]: ").strip()
     if move in [str(i) for i in range(1, 10)]:
         return divmod(int(move) - 1, 3)
     print("Invalid input.")
-    return getMove()
+    return get_player_move()
 
 
-def toContinue():
-    toCon = input("\nDo you want to play another game?\n[Yes: Y]/[No: N]: ").strip().lower()
-    if toCon in ["y", "n"]:
-        return toCon == "y"
+def is_continue_playing() -> bool:
+    """
+    Check if player wants to play a new game after one game ends
+    :return:
+    True: player wants to play a new game
+    False: player wants to quit
+    """
+    to_con = input("\nDo you want to play another game?\n[Yes: Y]/[No: N]: ").strip().lower()
+    if to_con in ["y", "n"]:
+        return to_con == "y"
     print("Invalid input. Please enter \"Y\" or \"N\".")
-    return toContinue()
+    return is_continue_playing()
 
 
-def updateMove(game, row, col):
-    game.makeMove(row, col)
-    game.printBoard()
-    return game.getResult(row, col)
+def update_move(game, row, col):
+    """
+    make the move and print updated game board and
+    :param game:
+    :param row:
+    :param col:
+    :return: game result:
+    None: game is still going
+    str: game result message
+    """
+    game.make_move(row, col)
+    game.print_board()
+    return game.get_result(row, col)
 
 
-def playWithAI(diff, isAITurn):
+def play_with_ai(diff, is_ai_turn):
+    """
+    start a new game and let player play with AI
+    when game ended print the result
+    :param diff: difficulty of AI
+    :param is_ai_turn: is AI playing first turn
+    :return: None
+    """
     game = Game()
-    game.printBoard()
-    while not game.isEnded:
-        row, col = game.solver(diff) if isAITurn else getMove()
-        isAITurn ^= True
-        res = updateMove(game, row, col)
+    game.print_board()
+    while not game.is_ended:
+        row, col = game.solver(diff) if is_ai_turn else get_player_move()
+        is_ai_turn ^= True
+        res = update_move(game, row, col)
     print(res)
 
 
-def playWithHuman():
+def play_with_human():
+    """
+    start a new game and let player play with another player
+    print result when game ended
+    :return: None
+    """
     game = Game()
-    game.printBoard()
-    while not game.isEnded:
-        row, col = getMove()
-        res = updateMove(game, row, col)
+    game.print_board()
+    while not game.is_ended:
+        row, col = get_player_move()
+        res = update_move(game, row, col)
     print(res)
 
 
 if __name__ == '__main__':
     print(colored("\nWelcome to Tic-Tac-Toe\n", "red"))
-    toCon = True
-    while toCon:
-        withAI = getAI()
+    to_continue = True
+    while to_continue:
+        withAI = get_ai()
         if withAI:
-            playWithAI(getDifficulty(), getAITurn())
+            play_with_ai(get_difficulty(), get_ai_turn())
         else:
-            playWithHuman()
-        toCon = toContinue()
+            play_with_human()
+        to_continue = is_continue_playing()
     print("\nThank you for playing. Bye!")
